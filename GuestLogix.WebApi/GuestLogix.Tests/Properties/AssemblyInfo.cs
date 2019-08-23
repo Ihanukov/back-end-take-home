5ec82fdfbfe63b96732843caef31971b375c89fa
Ina Hanukov <ihanukov@ideallifeonline.com>
2019-08-22 21:20:34 -0400

    Path Solution

---
+++ GuestLogix.WebApi/GuestLogix.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuestLogix.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuestLogix.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8979b35-9317-4c92-b3c5-a71c05744639")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GuestLogix.WebApi/GuestLogix.Tests/RoutesControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using GuestLogix.Business.Models.Routes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace GuestLogix.Tests
+{
+    [TestClass]
+    public class RoutesControllerTest 
+    {
+       
+        string baseURL = "http://localhost:58925/api/routes";
+
+
+        [TestMethod]
+        public void Routes_Get()
+        {
+            using (var client = new HttpClient())
+            {
+
+                var message = new HttpRequestMessage(HttpMethod.Get, this.baseURL+ "?origin=yyz&destination=yvr");
+                message.Headers.Add("Accept", "application/json");
+           
+
+                var response = client.SendAsync(message, HttpCompletionOption.ResponseContentRead).Result.Content.ReadAsStringAsync().Result;
+
+                var result = JsonConvert.DeserializeObject<Routes>(response);
+               
+                ValidateResult(result);
+
+            }
+        }
+        private void ValidateResult(Routes result)
+        {
+            Assert.IsNull(result, "No Path");
+            Assert.AreEqual(HttpStatusCode.OK, result.Path);
+            
+        }
+    }
+}
